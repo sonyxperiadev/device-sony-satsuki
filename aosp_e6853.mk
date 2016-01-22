eadb82dd593bc38bf044b54b11d0489a6612cfdc
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-01-22 13:26:42 +0100

    update sensor config path
    move sensor config to unified sensor HAL path /etc
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- aosp_e6853.mk
+++ aosp_e6853.mk
@@ -34,7 +34,7 @@ PRODUCT_COPY_FILES += \
     device/sony/satsuki/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
     device/sony/satsuki/rootdir/system/etc/BCM4356.hcd:system/etc/firmware/BCM43xx.hcd \
     device/sony/satsuki/rootdir/system/etc/wifi/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
-    device/sony/satsuki/rootdir/system/etc/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf \
+    device/sony/satsuki/rootdir/system/etc/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
     device/sony/satsuki/rootdir/system/etc/thermanager.xml:system/etc/thermanager.xml \
     device/sony/satsuki/rootdir/system/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
     device/sony/satsuki/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
